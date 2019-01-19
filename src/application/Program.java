d3535268ba594439c0b6a82e91ce265c41ec13be
Andre Dias <and.ddias@gmail.com>
2019-01-18 22:18:14 -0200

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Department department = new Department(1, "Financeiro");
-		
-		Seller seller = new Seller(10, "ANDRÉ LUIZ", "and.ddias@gmail.com", sdf.parse("27/06/1983"), 3007.0, department);
+	public static void main(String[] args) throws ParseException {		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
